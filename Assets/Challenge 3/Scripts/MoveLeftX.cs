94338193ad8d92c97c7fbce6a50f1a2b7f445f57
DGhevariya26 <darshitghevariya26@gmail.com>
2023-05-24 14:59:16 +0530

    Initial Commit

---
+++ Assets/Challenge 3/Scripts/MoveLeftX.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveLeftX : MonoBehaviour
+{
+    public float speed = 20;
+    private PlayerControllerX playerControllerScript;
+    private float leftBound = -10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerControllerScript = GameObject.Find("Player").GetComponent<PlayerControllerX>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {       
+        if (playerControllerScript.gameOver == false)
+        {
+            transform.Translate(Vector3.left * speed * Time.deltaTime);
+        }
+
+        if (transform.position.x < leftBound && gameObject.CompareTag("Bomb"))
+        {
+            Destroy(gameObject);
+        }
+
+        if (transform.position.x < leftBound && gameObject.CompareTag("Money"))
+        {
+            Destroy(gameObject);
+        }
+
+    }
+}
